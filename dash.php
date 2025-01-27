c79ace076d25bbb1ffdb23b3e0ba2bc68250cc53
neninex <74602434+neninex@users.noreply.github.com>
2025-01-27 07:47:02 +0700

    Add files via upload

---
+++ dash.php
@@ -0,0 +1,10 @@
+<?php
+include('config/koneksi.php');
+$config['judul_sub_halaman'] = "Dashboard </a>
+";
+$config['hal_aktif'] = "home";
+include('header.php');
+echo "<h2>Selamat Datang di ".$config['nama_website']."</h2>";
+echo "Silahkan pilih menu disamping untuk melanjutkan";
+include('footer.php');
+?>
